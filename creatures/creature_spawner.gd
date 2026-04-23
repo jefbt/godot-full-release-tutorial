93182c26bc11da5e1d56c250da72ed2540f1a083
Jeferson Tomazella <jefbt@outlook.com>
2026-04-22 22:29:28 -0300

    adds spawn creature visual feedback

--- creatures/creature_spawner.gd
+++ creatures/creature_spawner.gd
@@ -2,6 +2,7 @@ class_name CreatureSpawner extends Node2D
 
 @onready var spawned: Node2D = $Spawned
 @onready var spawn_timer: Timer = $SpawnTimer
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 const CREATURE_SIMPLE = preload("res://creatures/creature_simple.tscn")
 const CREATURE_FOLLOW = preload("res://creatures/creature_follow.tscn")
@@ -13,14 +14,19 @@ const CREATURE_FLY = preload("res://creatures/creature_fly.tscn")
 @export var spawn_time: float = 2.0
 
 func _ready() -> void:
-	spawn()
+	animated_sprite.visible = false
+	spawn(true)
 
 func _process(_delta: float) -> void:
 	if spawn_timer.is_stopped() and spawned.get_child_count() < max_creatures_spawned:
 		spawn_timer.start(spawn_time)
 
-func spawn() -> void:
-	# TODO add a pre-spawn effect (visual feedback so the player can see they coming)
+func spawn(ignore_animation: bool = false) -> void:
+	if not ignore_animation:
+		animated_sprite.visible = true
+		animated_sprite.play("default")
+		await animated_sprite.animation_finished
+		animated_sprite.visible = false
 	var creature: Creature = null
 	match (type):
 		Creature.Type.FOLLOW:
